6eb26fe18d494e0b9993c76fa478121937f8eaa2
joysofia <sonnejoy@gmail.com>
2017-10-13 15:29:34 +0800

    Hi this is Joy

---
+++ joy.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# ## Any Code ... just for test    
+# ### Let's say... 
+
+# In[5]:
+
+title = 'start to learn'
+sub ='learn hard if you can'
+firstcondition = 'BE HAPPY!'
+rule2= 'IT IS FINE TO TAKE A BREAK but not a stop'
+ALL = title + sub + "\n" + firstcondition + "\n" + rule2
+print(ALL)
+
+
+# In[6]:
+
+get_ipython().system('pwd')
+
+
+# In[ ]:
+
+
+
