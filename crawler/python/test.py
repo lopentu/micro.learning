f3e61de2209f884648c993170d92cd73b687d337
Yu-Yun <yuyun.unita@gmail.com>
2017-10-13 16:26:21 +0800

    delete confilect taco

--- crawler/python/test.py
+++ crawler/python/test.py
@@ -8,7 +8,6 @@
 
 'hererererereere taco taco taco good good'
 'where is everyone lalalalalalalala where are you'
-c2d7915d61733742feb8214a63e0794403732b33
 
 # I want conflict!
 'hererererereere taco taco taco good good'
@@ -16,7 +15,5 @@ c2d7915d61733742feb8214a63e0794403732b33
 
 
 'sabrina 好奇怪'
- 3dc2893a6b514ed9de6fceade6a5278b2fd2f301
 
 'sabrina 好奇怪的衝突'
-2d08065187053af4ad3182006e59a5891e7df57e
